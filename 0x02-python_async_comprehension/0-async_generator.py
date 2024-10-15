ac231427a0e109ce32b96a29b5a912ac85fafe39
elotwo <obiagwu94@gmail.com>
2024-10-15 23:54:55 +0100

    comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,13 +4,14 @@ this code is about adynchorous comprehesion
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     this function is a coroutine that return loop
     10 times
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
