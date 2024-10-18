b7189e2b757d94a85953421cb2af040583a9e876
elotwo <obiagwu94@gmail.com>
2024-10-18 22:52:07 +0100

    update

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -5,9 +5,9 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n, max_delay) ->float:
+async def measure_time(n, max_delay) -> float:
     """
-    This return the total time 
+    This return the total time
     """
     start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
+"""
+this is a regular function that returns asyncio.task
+"""
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
-def task_wait_random(max_delay: int):
-    wait_random(max_delay)
 
+
+def task_wait_random(max_delay: int):
+    """
+    return class
+    """
+    return (asyncio.Task(wait_random(max_delay)))
