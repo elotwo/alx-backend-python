00f5a46256f96421e19e8667012598d2f9c3e9f2
elotwo <obiagwu94@gmail.com>
2024-11-05 23:12:33 +0100

    updated

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -35,6 +35,8 @@ def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
         if not isinstance(nested_map, Mapping):
             raise KeyError(key)
         nested_map = nested_map[key]
+        
+    return nested_map
 
 def get_json(url: str) -> Dict:
     """Get JSON from remote URL.
